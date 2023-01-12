57535867df9d7af255ac42103ccd581244c1bd14
Jim Safley <jimsafley@gmail.com>
2023-01-12 16:14:17 -0500

    Add site setting

--- Module.php
+++ Module.php
@@ -4,13 +4,14 @@ namespace OutputFormats;
 use Omeka\Module\AbstractModule;
 use Laminas\EventManager\Event;
 use Laminas\EventManager\SharedEventManagerInterface;
-use Laminas\Mvc\Controller\AbstractController;
-use Laminas\ServiceManager\ServiceLocatorInterface;
-use Laminas\View\Renderer\PhpRenderer;
 
 class Module extends AbstractModule
 {
+    /**
+     * @var array List of available output format selectors
+     */
     protected $selectors = [
+        // Admin selectors
         [
             'resource' => 'items',
             'controller' => 'Omeka\Controller\Admin\Item',
@@ -39,6 +40,27 @@ class Module extends AbstractModule
         [
             'resource' => 'media',
             'controller' => 'Omeka\Controller\Admin\Media',
+            'event' => 'view.show.after',
+        ],
+        // Site selectors
+        [
+            'resource' => 'items',
+            'controller' => 'Omeka\Controller\Site\Item',
+            'event' => 'view.browse.after',
+        ],
+        [
+            'resource' => 'items',
+            'controller' => 'Omeka\Controller\Site\Item',
+            'event' => 'view.show.after',
+        ],
+        [
+            'resource' => 'item_sets',
+            'controller' => 'Omeka\Controller\Site\ItemSet',
+            'event' => 'view.browse.after',
+        ],
+        [
+            'resource' => 'item_sets',
+            'controller' => 'Omeka\Controller\Site\ItemSet',
             'event' => 'view.show.after',
         ],
     ];
@@ -48,29 +70,28 @@ class Module extends AbstractModule
         return include sprintf('%s/config/module.config.php', __DIR__);
     }
 
-    public function install(ServiceLocatorInterface $services)
-    {
-    }
-
-    public function uninstall(ServiceLocatorInterface $services)
-    {
-    }
-
-    public function getConfigForm(PhpRenderer $renderer)
-    {
-    }
-
-    public function handleConfigForm(AbstractController $controller)
-    {
-    }
-
     public function attachListeners(SharedEventManagerInterface $sharedEventManager)
     {
+        /**
+         * Render output format selectors.
+         */
         foreach ($this->selectors as $selector) {
             $sharedEventManager->attach(
                 $selector['controller'],
                 $selector['event'],
                 function (Event $event) use ($selector) {
+                    // Check if this selector should be rendered.
+                    $services = $this->getServiceLocator();
+                    $status = $services->get('Omeka\Status');
+                    if ($status->isSiteRequest()) {
+                        $addSelectorsSite = $services
+                            ->get('Omeka\Settings\Site')
+                            ->get('output_formats_add_selectors_site');
+                        if (!$addSelectorsSite) {
+                            return;
+                        }
+                    }
+                    // Render the selector.
                     $view = $event->getTarget();
                     $view->headScript()->appendFile($view->assetUrl('js/output-formats.js', 'OutputFormats'));
                     echo $view->partial('common/output-formats-format-selector', [
@@ -89,5 +110,34 @@ class Module extends AbstractModule
                 }
             );
         }
+        /**
+         * Add site settings.
+         */
+        $sharedEventManager->attach(
+            'Omeka\Form\SiteSettingsForm',
+            'form.add_elements',
+            function (Event $event) {
+                $services = $this->getServiceLocator();
+                $siteSettings = $services->get('Omeka\Settings\Site');
+                $form = $event->getTarget();
+
+                $elementGroups = $form->getOption('element_groups', []);
+                $elementGroups['output_formats'] = 'Output Formats'; // @translate
+                $form->setOption('element_groups', $elementGroups);
+
+                $form->add([
+                    'type' => 'checkbox',
+                    'name' => 'output_formats_add_selectors_site',
+                    'options' => [
+                        'element_group' => 'output_formats',
+                        'label' => 'Add output format selector to resource pages',
+                    ],
+                    'attributes' => [
+                        'value' => $siteSettings->get('output_formats_add_selectors_site'),
+                    ],
+                ]);
+
+            }
+        );
     }
 }
